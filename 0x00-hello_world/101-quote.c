1e306910ad31d87d6d6f33cb6e8cb5489396de88
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-04 04:41:13 +0000

    Command write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *gcc = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *gcc = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, gcc, 59);
 	return (1);
