55be72707c1dadb7667e42e43ac86bb3b990f307
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-05 03:47:17 +0000

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n > 97)
+	{
+		putchar(n);
+		n = n - 1;
+	}
+	putchar('\n');
+	return (0);
+}
