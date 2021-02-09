2b23cb99f23d284a5da1377d24abcf6befef98ec
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-09 16:42:44 +0000

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_sign - this command print the sign 
+ *@c: the caracter print
+ *
+ * Return: int
+ */
+int print_sign(int n)
+{
+
+
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -33,4 +33,10 @@ int _isalpha(int c);
 
 #endif
 
+#ifndef PRINT_SIGN_H
+#define PRINT_SIGN_H
+
+int print_sign(int n);
+
+#endif
 
