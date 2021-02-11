75c737b37b69da980c3892532b23e12783c6cd5b
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-11 22:56:55 +0000

    rints 10 times the numbers, from 0 to 14Wq

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,11 @@ void more_numbers(void)
 	{
 		for (c = 0; c < 15; c++)
 		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + 48);
+			}
 			_putchar((c % 10) + 48);
-			_putchar((c / 10) + 48);
 		}
 		_putchar('\n');
 	}
