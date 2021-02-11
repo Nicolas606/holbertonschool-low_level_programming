d843394561d663d8d02dc28109c90239cee272d7
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-11 14:38:22 +0000

    Add the holberton.h file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
+int _putchar(char c);
+
+#endif
+
+#ifndef _ISUPPER_H
+#define _ISUPPER_H
+
+int _isupper(int c);
+
+#endif
+
+#ifndef _ISDIGIT_H
+#define _ISDIGIT_H
+
+int _isdigit(int c);
+
+#endif
+
