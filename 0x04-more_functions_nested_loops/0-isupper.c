cf505ff70568196a1eb497a0cc42da80549facb7
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-11 14:34:21 +0000

    identify capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - this command find the lowcaser
+ *@c: the caracter print
+ *
+ * Return: int
+ */
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
