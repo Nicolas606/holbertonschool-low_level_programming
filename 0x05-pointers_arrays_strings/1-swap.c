e04c3a2bfde922f0f9d0e7b219a824744e6ff1c5
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-16 15:12:18 +0000

    correct the parameter of member a and b

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_numbers - function that swaps the values of two integers
- * @*a: 
- * @*b:
+ * swap_int - function that swaps the values of two integers
+ * @a: variable that the exercise gives us
+ * @b: variable that the exercise gives us
  * Return: int
  */
 void swap_int(int *a, int *b)
 {
 	int c = *b;
-	
+
 	*b = *a;
 	*a = c;
 }
