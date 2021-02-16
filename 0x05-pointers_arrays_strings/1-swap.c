19f813f76cf407e98824345062de1bd4249d4716
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-16 15:02:52 +0000

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,26 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_numbers - this command find the lowcaser
- *
- *
+ * print_numbers - function that swaps the values of two integers
+ * @*a: 
+ * @*b:
  * Return: int
  */
 void swap_int(int *a, int *b)
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	int c = *b;
+	
+	*b = *a;
+	*a = c;
 }
-
