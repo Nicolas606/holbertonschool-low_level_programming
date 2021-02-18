542ca8df4b9ee7598384051a566bdd194dcdfc22
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-18 17:44:43 +0000

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton"
+
+/**
+ * _strcat-function that concatenates two strings.
+ * @dest: variable that the exercise gives us
+ * @src: variable that the exercise gives us
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *i;
+	char *p;
+
+	for (i = dest; *i != '\0'; i++)
+		;
+
+	for (p = src; *p != '\0'; p++, i++)
+		*i = *p;
+
+	*p = '\0';
+
+	return (dest);
+}
