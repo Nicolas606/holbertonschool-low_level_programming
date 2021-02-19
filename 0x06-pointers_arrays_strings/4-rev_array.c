201cc7933c7b6569c2cf144b3460bf74a17de801
Nicolas Castillo <castillonicolas606@gmail.com>
2021-02-19 04:21:00 +0000

    function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * string_toupper - function that changes all lowercase letters of a
- * string to uppercase.
- * @p: variable that we enter as a string
+ * reverse_array - function that reverses the content of an array of integers.
  *
+ * @a: Array
+ * @n: number the elements of array
  *
  * Return: char
  */
