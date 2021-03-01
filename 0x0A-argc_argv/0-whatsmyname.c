324ff0bbde27294035d79df450a7ee090d8b941c
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-01 01:23:05 +0000

    corret the description of main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - Entry point.
  * @argc: The number of command line arguments and The size of the argv array.
  * @argv: An array containing the program command line arguments and An array.
  * of size argc.
  *
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
