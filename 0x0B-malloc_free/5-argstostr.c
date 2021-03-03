fa92658f1af29833ac68437348305e4a97f6a97a
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-03 04:44:38 +0000

    function that concatenates all the arguments of your program.

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenates all the arguments of your program.
+ * created by your alloc_grid function.
+ *
+ * @ac: number of arguments.
+ * @av: array of argument.
+ *
+ * Return: s
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+int *s;
+
+if(ac == 0 || av == 0)
+return (0);
+
+
+
+return (s);
+}
