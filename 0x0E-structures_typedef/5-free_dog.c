82ef9b85033955353d4176e3de0595fa693d6cc2
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-09 04:22:56 +0000

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - Frees dogs.
+*
+* @d: dog's adress.
+*
+* Return: Void.
+*/
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
