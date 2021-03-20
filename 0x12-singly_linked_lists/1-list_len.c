742fc14b6df678aa35bcd6d4735ffdcfc8e58796
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-20 02:03:33 +0000

    upload the file 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t
+ *  list.
+ *
+ * @h: Pointer to list header.
+ *
+ * Return:  number of elements
+ *
+ **/
+
+size_t list_len(const list_t *h)
+{
+	size_t num_of_el;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		num_of_el++;
+		h = h->next;
+	}
+	return(num_of_el);
+}
