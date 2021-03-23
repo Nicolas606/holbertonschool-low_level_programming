5dde7910de9255b12848a1cba81d72fea847aa75
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-23 02:05:20 +0000

    Upload the file 0-print_listint.c and the file lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: Pointer to list header.
+ *
+ * Return: Size of linked list
+ *
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	if (h == 0)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->n != 0)
+			printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
