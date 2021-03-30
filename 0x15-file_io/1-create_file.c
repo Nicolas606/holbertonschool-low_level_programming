257ff8063a9d9346016179ed47262d6b020b2258
Nicolas606 <vastillonicolas606@gmail.com>
2021-03-30 19:22:06 +0000

    fix  if text_content == NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,12 +25,12 @@ int create_file(const char *filename, char *text_content)
 	;
 
 	if (text_content == NULL)
-	{	/* return (1); */
+		return (1); 
 
 	file_w = write(file_description, text_content, counter);
 	if (file_w == -1)
 		return (-1);
-		}
+		
 
 	close(file_description);
 	return (1);
