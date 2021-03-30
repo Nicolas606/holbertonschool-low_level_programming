d368b2fd3a7bde773d972aaf66dac15430ea5bd7
Nicolas606 <vastillonicolas606@gmail.com>
2021-03-30 01:10:41 +0000

    Fix Betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
-* read_textfile - function that appends text at the end of a file.
+* create_file - function that creates a file.
 *
-* @filename: name of file
-* @letters: size of buffer
+* @filename: name of file.
+* @text_content: text contained in the file.
 *
-* Return: size of characters
+* Return: 1 on success, -1 on failure.
 */
 
 int create_file(const char *filename, char *text_content)
@@ -16,18 +16,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	file_description = open(filename, O_CREAT |	O_RDWR | O_TRUNC, 0600 );
+	file_description = open(filename, O_CREAT |	O_WRONLY | O_TRUNC, 0600);
 	if (file_description == -1)
 		return (-1);
 
-	for ( counter = 0; text_content[counter]; counter++)
+	for (counter = 0; text_content[counter]; counter++)
 	;
 
 	file_w = write(file_description, text_content, counter);
 	if (file_w == -1)
-	return(-1);
+	return (-1);
 
-	close (file_description);
+	close(file_description);
 	return (1);
 
 }
