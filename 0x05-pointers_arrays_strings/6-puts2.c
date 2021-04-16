d91455209fdcfa6705701f6a07acafd924ed9178
Nicolas <castillonicolas606@gmail.com>
2021-04-16 20:03:24 +0000

    Fixx betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 {
 	int i = 0, j;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 		i++;
 
-	for(j = 0; j <= i -1; j = j +2)
+	for (j = 0; j <= i - 1; j = j + 2)
 		_putchar(str[j]);
 
 	_putchar('\n');
