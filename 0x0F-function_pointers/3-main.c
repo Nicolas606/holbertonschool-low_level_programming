e8b75a17b3dc73ba4247442acb4b36f47ff19edb
Nicolas606 <castillonicolas606@gmail.com>
2021-09-08 19:57:38 -0500

    Fix the return

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	if (argv[2][1] != 0)
 	{
-		printf("Erro\n");
+		printf("Error\n");
 		exit(99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
